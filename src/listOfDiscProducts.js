75226fd099f4d8e4bc0ab9787ad4c4c5537659e1
ChildOfLigth <155371765+ChildOfLigth@users.noreply.github.com>
2024-06-08 19:02:07 +0300

    Add files via upload

---
+++ src/listOfDiscProducts.js
@@ -0,0 +1,23 @@
+export const listOfDiscProducts = [
+  {
+    product: "Xiaomi Smart Band 8 Black",
+    imgSrc: "../imgs/367323210.webp",
+    discountPrice: "236грн",
+    defaultPrice: "279грн",
+    func: "Отслеживание сна, Пройденное расстояние, Содержание кислорода в крови, Шагомер",
+  },
+  {
+    product: "Xiaomi Mi Smart Band 7 NFC Black",
+    imgSrc: "../imgs/272023891.webp",
+    discountPrice: "290грн",
+    defaultPrice: "350грн",
+    func: "Контроль дневной активности, Отслеживание сна, Пульсометр, Секундомер, Содержание кислорода в крови, Счетчик калорий, Фитнес-тренер",
+  },
+  {
+    product: "Lemfo C6T",
+    imgSrc: "../imgs/188525068.webp",
+    discountPrice: "236грн",
+    defaultPrice: "279грн",
+    func: "Пройденное расстояние, Пульсометр, Секундомер, Скорость движения, Содержание кислорода в крови, Счетчик калорий, Умный будильник, Функция плавания, Шагомер",
+  },
+];
