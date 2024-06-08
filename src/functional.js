5ced20fd4b8bfde46cdec8b1fb749317c56304a5
ChildOfLigth <155371765+ChildOfLigth@users.noreply.github.com>
2024-06-08 19:01:17 +0300

    Create functional.js

---
+++ src/functional.js
@@ -0,0 +1,103 @@
+const headerElem = document.querySelector(".header");
+const buttonShowConnWindow = document.querySelectorAll(".showConnectionBlock");
+const form = document.forms.submittingTheForm;
+const nameInp = form.name;
+const numberInp = form.numberPhone;
+const emailInp = form.email;
+const closeMdlWindow = document.getElementById("closeModalWind");
+const saveDataBtn = document.getElementById("saveDataUser");
+const showDataUser = document.createElement("div");
+showDataUser.className = "idntityConfirmResponse";
+
+buttonShowConnWindow.forEach((buttElem) => {
+  buttElem.onclick = () => {
+    form.classList.add("_active");
+  };
+});
+
+closeMdlWindow.onclick = () => form.classList.remove("_active");
+document.onkeydown = (event) => {
+  if (event.key == "Escape") {
+    form.classList.remove("_active");
+  }
+};
+
+saveDataBtn.onclick = () => {
+  const valueName = nameInp.value;
+  const valueNumberPh = numberInp.value;
+  const valueEmail = emailInp.value;
+
+  if (valueName && valueEmail && valueNumberPh) {
+    showDataUser.innerHTML = `
+      <p><span>${valueName}</span>, ваша заявка принята! Мы пришлем Вам сообщение на почту или позвоним на указанный Вами номер.</p>
+    `;
+    showDataUser.classList.add("_active");
+    nameInp.value = "";
+    numberInp.value = "";
+    emailInp.value = "";
+    headerElem.appendChild(showDataUser);
+    form.classList.remove("_active");
+  } else {
+    showDataUser.innerHTML = `Введите данные во все поля.`;
+    showDataUser.classList.add("_active");
+    headerElem.appendChild(showDataUser);
+  }
+  setTimeout(() => showDataUser.classList.remove("_active"), 4000);
+};
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+document.addEventListener("scroll", () => {
+  const advBlock = document.querySelector(".advantagesBlock");
+  const targetPosition = advBlock.getBoundingClientRect();
+
+  if (targetPosition.top >= 540) {
+    advBlock.classList.add("__show");
+  }
+});
+
+///////////////////////////////////////////////////////////////////////////
+const imgs = document.querySelectorAll(".discountedProductsBlock__slider_img");
+const controlls = document.querySelectorAll(".controlls");
+let imgIndex = 0;
+
+const show = (index) => {
+  imgs[imgIndex].classList.remove("active");
+  imgs[index].classList.add("active");
+  imgIndex = index;
+};
+
+controlls.forEach((e) => {
+  e.addEventListener("click", (event) => {
+    if (event.currentTarget.classList.contains("prev")) {
+      let index = imgIndex - 1;
+
+      if (index < 0) {
+        index = imgs.length - 1;
+      }
+      show(index);
+    } else if (event.currentTarget.classList.contains("next")) {
+      let index = imgIndex + 1;
+
+      if (index >= imgs.length) {
+        index = 0;
+      }
+      show(index);
+    }
+  });
+});
+
+show(imgIndex);
+//////////////////////////////////////////////////////////////
+const toTopBtn = document.getElementById("arrowStart");
+toTopBtn.onclick = () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+};
